daf90213b2cc3a64d5aa3162d53a5af5a33dfb09
Alex080190 <Alex080190@mail.ru>
2023-01-28 18:02:03 +0400

    Initial commit

---
+++ src/main/java/com/example/jwttestagain/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.jwttestagain.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/com/example/jwttestagain/JwtTestAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jwttestagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTestAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
